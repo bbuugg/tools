19a59d6ff62b76c1e55ac125fabb18983c58689b
chengyao <chengyao0320@foxmail.com>
2025-08-24 22:37:16 +0800

    update

--- src/tools/WebRtcFileTransfer.vue
+++ src/tools/WebRtcFileTransfer.vue
@@ -440,12 +440,14 @@ const initWebRTC = async (isOfferer: boolean = true) => {
         {
           urls: [
             'turn:turn.codeemo.cn:3478',
-            'stun:stun.l.google.com:19302',
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
-            'stun:stun3.l.google.com:19302',
-            'stun:stun4.l.google.com:19302',
+            // 'stun:stun.l.google.com:19302',
+            // 'stun:stun1.l.google.com:19302',
+            // 'stun:stun2.l.google.com:19302',
+            // 'stun:stun3.l.google.com:19302',
+            // 'stun:stun4.l.google.com:19302',
           ],
+          username: 'codeemo',
+          credential: 'codeemo',
         },
       ],
     })
